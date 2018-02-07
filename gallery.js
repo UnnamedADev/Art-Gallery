ca0aae47366866d0118678bc83ae1386d098d224
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-02-07 03:03:08 +0100

    v0.0.3
    STRUCTURE
    + Added new noise texture files
    + Added new example images
    HTML
    + Modified images tiles format
    + Added preview section
    JAVASCRIPT
    + Added code that provides sliding down and sliding up preview window with image
    + Added click detection for closing preview window when user clicked out of image
    CSS
    + Improved style for new elements

--- gallery.js
+++ gallery.js
@@ -3,12 +3,44 @@
 $(document).ready(function(){
     
     //const
-    
-    
+    const anmDuration = 500;
+    const plsDuration = 1000;
     //vars
     
     //main
+    paintingsEvents();
+    closeEvent();
     
     //functions
+    function paintingsEvents(){
+        $(".painting").each(function(){
+            
+            $(this).click(function(){
+                
+                var myHref = $(this).attr("src");
+                
+                $(".preview img").attr("src", myHref);
+                $(".preview").animate({
+                    top: 0
+                },anmDuration,"swing");
+            });
+
+        });
+    }
+    
+    function closeEvent(){
+        $(".preview").click(function(){
+            
+            var myPic = $(".preview img")[0];
+            var trgt = $(event.target)[0];
+            
+            if(myPic != trgt){
+                $(this).animate({
+                    top: "-100%"
+                },anmDuration,"swing");
+            }
+            
+        });
+    }
     
 });
